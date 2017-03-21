f6ee21d0448cf21b52136aa8b24ec249876023b9
Daniel Llinas <danllinas@gmail.com>
2017-03-20 20:50:48 -0700

    Added missing files before diving in

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Creating Articles" do
+  scenario "A user creates a new article" do
+    #visit root page
+    visit "/"
+
+    #click on new article
+    click_link "New Article"
+
+    #fill in title
+    fill_in "Title", with: "Creating a blog"
+
+    #fill in body
+    fill_in "Body", with: "Lorem Ipsum"
+
+    #click a button 'create article'
+    click_button "Create Article"
+
+    #Should get a Article has been created
+    expect(page).to have_content("Article has been created")
+
+    #redirect to a page
+    expect(page.current_path).to eq(articles_path)
+
+  end
+end
